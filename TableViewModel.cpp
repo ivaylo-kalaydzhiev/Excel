65313d67cf260b56b2c5c9c10eb63098b2dc7bf1
Ivaylo Kalaydzhiev <ivailo.s.k@outlook.com>
2025-05-25 16:08:11 +0300

    Implement TableViewModel

--- TableViewModel.cpp
+++ TableViewModel.cpp
@@ -1,1 +1,54 @@
 #include "TableViewModel.h"
+
+TableViewModel::TableViewModel(TableConfiguration config, TableModel tableModel)
+    : configuration(std::move(config)), tableModel(std::move(tableModel)) {
+    updateAllDisplayableCells();
+}
+
+void TableViewModel::handle(const Event& event) {
+    std::visit([this](const auto& e) {
+        using T = std::decay_t<decltype(e)>;
+
+        if constexpr (std::is_same_v<T, InsertEvent>) {
+            //tableModel.setCellValue(e.target, CellValue{ e.value });
+            updateDisplayableCell(e.target);
+        }
+        else if constexpr (std::is_same_v<T, DeleteEvent>) {
+            tableModel.removeCellValue(e.target);
+            displayableTableModel.removeDisplayValue(e.target);
+        }
+        else if constexpr (std::is_same_v<T, ReferenceEvent>) {
+            tableModel.setCellValue(e.target, CellValue{ e.source });
+            updateDisplayableCell(e.target);
+        }
+        else if constexpr (std::is_same_v<T, FormulaEvent>) {
+            tableModel.setCellValue(e.target, CellValue{ FormulaValue{e.formula, e.params} });
+            updateDisplayableCell(e.target);
+        }
+        }, event);
+}
+
+const TableConfiguration& TableViewModel::getConfiguration() const {
+    return configuration;
+}
+
+const DisplayableTableModel& TableViewModel::getDisplayableTableModel() const {
+    return displayableTableModel;
+}
+
+void TableViewModel::updateDisplayableCell(const CellAddress& address) {
+    const CellValue* cellValue = tableModel.getCellValue(address);
+    if (cellValue) {
+        CellEvaluator evaluator(tableModel);
+        displayableTableModel.setDisplayValue(address, evaluator.evaluate(*cellValue));
+    }
+}
+
+void TableViewModel::updateAllDisplayableCells() {
+    const auto& allCells = tableModel.getAllCells();
+    CellEvaluator evaluator(tableModel);
+
+    for (const auto& [address, value] : allCells) {
+        displayableTableModel.setDisplayValue(address, evaluator.evaluate(value));
+    }
+}
--- TableViewModel.h
+++ TableViewModel.h
@@ -1,5 +1,25 @@
 #pragma once
-class TableViewModel
-{
-};
 
+#include "TableConfiguration.h"
+#include "TableModel.h"
+#include "DisplayableTableModel.h"
+#include "EventParser.h"
+#include "CellEvaluator.h"
+
+class TableViewModel {
+public:
+    TableViewModel(TableConfiguration config, TableModel tableModel);
+
+    void handle(const Event& event);
+
+    const TableConfiguration& getConfiguration() const;
+    const DisplayableTableModel& getDisplayableTableModel() const;
+
+private:
+    TableConfiguration configuration;
+    TableModel tableModel;
+    DisplayableTableModel displayableTableModel;
+
+    void updateDisplayableCell(const CellAddress& address);
+    void updateAllDisplayableCells();
+};
